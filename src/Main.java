50118c094082dd2d28208b724874db1cc6ff2b5e
JSkinny78 <Johntheblack99@gmail.com>
2018-10-15 12:28:20 -0500

    Made basic logic for some methods

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,13 @@ import java.util.Scanner;
 public class Main {
 	Scanner sc;
 	int length=0;
+	String currentString;
 	char currentChar;
-	
+	char[] lexArray = new char[] {'+', '=','?', '!', '<','>','-',':',')','('};
 	public static void main(String[] args) {
+		//Used for initial testing
 		
-		
+	
 		
 	}
 	
@@ -24,17 +26,18 @@ public class Main {
 		sc = new Scanner(str);
 		while(sc.hasNext())
 		{
-			currentChar = sc.next().charAt(0);
-			if(checkComment(currentChar)) {
-				defineComment(currentChar,sc);
-			}else if(checkString(currentChar)) {
-				defineString(currentChar,sc);
-			}else if(checkNumeric(currentChar)) {
-				defineNumeric(currentChar,sc);
-			}else if(checkLexeme(currentChar)) {
+			currentString = sc.next();
+			currentChar = currentString.charAt(0);
+			if(checkLexeme(currentChar)) {
 				defineLexeme(currentChar,sc);
+			}else if(checkComment(currentChar)) {
+				defineComment(currentString,sc); 
+			}else if(checkString(currentChar)) {
+				defineString(currentString,sc);
+			}else if(checkNumeric(currentChar)) {
+				defineNumeric(currentString,sc);
 			}else {
-				defineIdentifier(currentChar,sc);
+				defineIdentifier(currentString,sc);
 			}
 		
 		}
@@ -66,14 +69,23 @@ public class Main {
 	//Check set just determines which define method we use
 	
 	private Boolean checkLexeme(char c){
-		//Check if c is equal to any of the lexeme 
-		throw new UnsupportedOperationException();
+		//Check if c is equal to any of the lexeme
+		
+		for(int i=0; i<lexArray.length; i++)
+		{
+			if(c == lexArray[i])
+			{
+				return true;
+			}
+		}
+		return false;
+		//throw new UnsupportedOperationException();
 	}
 	//Only true if the current is a opening string "
 	//define string will close the string and print
 	private Boolean checkComment(char c) {
 		//Look for e char equals method
-		if(c == '"') 
+		if(c == '[') 
 			return true;
 		else
 			return false;
@@ -83,13 +95,16 @@ public class Main {
 	private Boolean checkString(char c)
 	{
 		//check if string
-		
-		throw new UnsupportedOperationException();
+		if(c == '\"') 
+			return true;
+		else
+			return false;
+		//throw new UnsupportedOperationException();
 	}
 	//Checks if c is equal to 0-9
 	private Boolean checkNumeric(char c) {
-		
-		throw new UnsupportedOperationException();
+		return Character.isDigit(c);
+		//throw new UnsupportedOperationException();
 	}
 	
 	//Define methods
@@ -98,27 +113,64 @@ public class Main {
 	private void defineLexeme(char c, Scanner sc){
 		//Return the description of Lexeme
 		//Switch statement to decide?
-		throw new UnsupportedOperationException();
+		switch(c)
+		{
+			case '+': print(c,"add/concatenate");
+			break;
+			case '=': print(c,"assignment");
+			break;
+			case '?': print(c,"is equal to");
+			break;
+			case '!': print(c,"is not equal to");
+			break;
+			case '<': print(c,"less than");
+			break;
+			case '>': print(c,"greater than");
+			break;
+			case '-': print(c,"negation");
+			break;
+			case ':': print(c,"colon");
+			break;
+			case '(': print(c,"open parenthesis");
+			break;
+			case ')': print(c,"close parenthesis");
+			break;
+			default: print(c,"**Error**");
+		
+		}
+		
+		//throw new UnsupportedOperationException();
 	}
 	//It takes the first " and then loops until the closing "
 	//Check each token for " stop and print when found
-	private void defineString(char c, Scanner sc)	{
+	private void defineString(String s, Scanner sc)	{
+		
+		
+		
+		
 		throw new UnsupportedOperationException();
 	}
 	//It takes the [ and loops until ]
 	//Check each token for [ stop and print when found
-	private void defineComment(char c, Scanner sc) {
+	private void defineComment(String s, Scanner sc) {
+		
+		throw new UnsupportedOperationException();
 	}
 	//takes the first number, if 0 it prints
 	//otherwise it loops until it finds a non number
 	//then prints the number as a string NO NEED TO MAKE IT AN INT
-	private void defineNumeric(char c, Scanner sc) {
+	private void defineNumeric(String s, Scanner sc) {
+		if(s == "0") {
+			print(s,"numeric literal");
+		}else {
+			
+		}
 		throw new UnsupportedOperationException();
 	}
 	//Need to work on the logic for this.
 	//It may need to continue to loop through checking for Lexemes
 	//Any Ideas?
-	private void defineIdentifier(char c, Scanner sc) {
+	private void defineIdentifier(String s, Scanner sc) {
 		throw new UnsupportedOperationException();
 
 	}
